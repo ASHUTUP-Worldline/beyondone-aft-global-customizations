e2e5186960134e520e04ab1370f30abbdeed03d3
Ashutosh Upadhyay <toashutoshupadhyay@gmail.com>
2025-10-27 14:05:01 +0800

    New default bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,15 @@ resource "aws_s3_account_public_access_block" "block_all_public" {
   ignore_public_acls  = true
   restrict_public_buckets  = true
 }
+
+data "aws_caller_identity" "current" {}
+
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = "default-bucket-${data.aws_caller_identity.current.account_id}"
+
+  tags = {
+    Name        = "Default bucket"
+  }
+}
